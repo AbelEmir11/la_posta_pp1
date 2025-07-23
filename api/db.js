c94fc27fede832c210f07b93e498856f351d69d0
Abel12440 <g1911032@gmail.com>
2025-07-23 10:59:32 -0300

    probamos con vercel

---
+++ api/db.js
@@ -0,0 +1,10 @@
+// api/db.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.SUPABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+export default pool;
